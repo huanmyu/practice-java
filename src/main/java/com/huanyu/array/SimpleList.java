cfbddb4686c5d937c354456048168c5b0e6afcf0
Bowen Chen <bowenchen886@gmail.com>
2017-11-23 21:49:58 +0800

    add simple list junit test

--- src/main/java/com/huanyu/array/SimpleList.java
+++ src/main/java/com/huanyu/array/SimpleList.java
@@ -6,7 +6,7 @@ public class SimpleList<T> {
     private Object[] elementData;
     private int size = 0;
 
-    private final static int DEFAULT_LIST_CAPACITY = 10;
+    private final static int DEFAULT_LIST_CAPACITY = 4;
 
     public SimpleList() {
         elementData = new Object[DEFAULT_LIST_CAPACITY];
@@ -26,7 +26,7 @@ public class SimpleList<T> {
     }
 
     public boolean isEmpty() {
-        return size > 0;
+        return size == 0;
     }
 
     public boolean add(T e) {
@@ -37,34 +37,72 @@ public class SimpleList<T> {
     }
 
     public boolean remove(Object o) {
-        for (Object element: elementData) {
-            int i = 0;
-            if (element == o) {
+        for (int i = 0; i < size; i++) {
+            if (elementData[i] == o) {
                 for (int j = i; j < size; j++) {
                     elementData[j] = elementData[j + 1];
                 }
+                size = size - 1;
+                elementData[size] = 0;
+                i--;
             }
-            i++;
         }
-        resize();
         return true;
     }
 
+    public boolean delete(int index) {
+        for (int i = index; i < size; i++) {
+            elementData[i] = elementData[i + 1];
+        }
+        size = size - 1;
+        elementData[size] = 0;
+        return true;
+    }
+
+    public int[] removeReturnIndex(Object o) {
+        StringBuilder s = new StringBuilder();
+        int i = 1;
+        for (Object element: elementData) {
+            if (element == o) {
+                s.append(i);
+                s.append(",");
+            }
+            i++;
+        }
+        s.deleteCharAt(s.length() - 1);
+        remove(o);
+        String[] sIndexs = s.toString().split(",");
+        int[] indexs = new int[sIndexs.length];
+        for (int j = 0; j < sIndexs.length; j++) {
+            indexs[j] = Integer.parseInt(sIndexs[j]);
+        }
+        return indexs;
+    }
+
     public T get(int index) {
-        resize();
-        return (T) elementData[index];
+        return (T) elementData[index-1];
     }
 
     private void resize() {
         if (elementData.length == 0) {
             elementData = new Object[DEFAULT_LIST_CAPACITY];
-        } else if (elementData.length == size) {
-            int newCap = 2 * size;
-            elementData = new Object[newCap];
-        } else if (elementData.length / 4 > size) {
-            int newCap = elementData.length / 2;
-            elementData = new Object[newCap];
+            return;
         }
+
+        int newCap = 0;
+        if (elementData.length == size) {
+            newCap = 2 * size;
+        } else if (elementData.length / 4 > size) {
+            newCap = elementData.length / 2;
+        } else {
+            return;
+        }
+
+        Object[] newElementData = new Object[newCap];
+        for (int i = 0; i < size; i++) {
+            newElementData[i] = elementData[i];
+        }
+        elementData = newElementData;
     }
 
     @Override
--- src/test/java/com/huanyu/array/SimpleListTests.java
+++ src/test/java/com/huanyu/array/SimpleListTests.java
@@ -2,13 +2,48 @@ package com.huanyu.array;
 
 import org.junit.Test;
 
+import static org.hamcrest.CoreMatchers.hasItems;
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
 
 public class SimpleListTests {
 
+    private static SimpleList<Integer> simpleList = new SimpleList<Integer>();
+
+    static {
+        simpleList.add(1);
+        simpleList.add(2);
+        simpleList.add(3);
+        simpleList.add(4);
+        simpleList.add(2);
+        simpleList.add(2);
+    }
+
     @Test
     public void sizeTest() {
-        SimpleList<Integer> simpleList = new SimpleList<Integer>();
-        assertEquals(simpleList.size(), 0);
+        assertEquals(simpleList.size(), 6);
+    }
+
+    @Test
+    public void isEmptyTest() {
+        assertEquals(simpleList.isEmpty(), false);
+    }
+
+    @Test
+    public void removeTest() {
+        simpleList.remove(2);
+        assertEquals(simpleList.size(), 3);
+    }
+
+    @Test
+    public void removeReturnIndexTest() {
+        int[] indexs = simpleList.removeReturnIndex(2);
+        assertArrayEquals(indexs, new int[]{2, 5, 6});
+    }
+
+    @Test
+    public void getTest() {
+        assertEquals((Object)simpleList.get(2), 2);
     }
 }
