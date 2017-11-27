48c9de3409167a29bc9061e09b91a9ada547f3d7
Bowen Chen <bowenchen@augmentum.com.cn>
2017-11-27 18:01:30 +0800

    add jms message handler

--- src/main/java/com/huanyu/main.java
+++ src/main/java/com/huanyu/main.java
@@ -1,17 +1,12 @@
 package com.huanyu;
 
-import com.huanyu.datetime.ExampleDateTime;
-
 import java.text.ParseException;
 
 public class main {
     public static void main(String[] args) throws ParseException {
+        //LocalTime currentTime = new LocalTime();
+        //System.out.println("The current local time is: " + currentTime);
+
         System.out.println("This is my practice java");
-        ExampleDateTime example = new ExampleDateTime();
-        example.addMonthWithCalendar("2017-04-28", 10);
-        example.addMonthWithCalendar("2017-04-29", 10);
-        example.addMonthWithCalendar("2017-04-30", 10);
-        example.addMonthWithCalendar("2017-11-30", 3);
-        example.addMonthWithCalendar(10);
     }
 }
---
+++ src/main/java/com/huanyu/message/Application.java
@@ -0,0 +1,4 @@
+package com.huanyu.message;
+
+public class Application {
+}
